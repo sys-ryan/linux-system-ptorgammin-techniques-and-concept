654c93adf4f1a8d32145edb31f85b4af466ccccb
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-11-13 23:47:58 +0900

    07. Build Project using Makefiles
    - Create common_math/common_math.h
    - Create common_math/common_math.c
    - Create main.c

---
+++ 07. Build Project using Makefiles/common_math/common_math.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON__MATH__
+#define __COMMON__MATH__
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double mul(double a, double b);
+double div(double a, double b);
+double max(double a, double b);
+double min(double a, double b);
+
+#endif /* __COMMON__MATH__ */
